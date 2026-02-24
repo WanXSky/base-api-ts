221175f946b53d3ff62b96f00fa6b216e67031ef
Onokosy <onokosy@hotmail.com>
2026-02-24 14:52:55 +0700

    Update userSchema.ts

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 
 export const registerSchema = z.object({
-  username: z.string({ required_error: 'Username wajib diisi' }).min(4, 'Username minimal 4 huruf'),
-  password: z.string({ required_error: 'Password wajib diisi' }).min(8, 'Password minimal 8 karakter'),
-  name: z.string({ required_error: 'Name wajib diisi' }).min(5, 'Name minimal 5 huruf')
+  username: z.string({ message: 'Username wajib diisi' }).min(4, 'Username minimal 4 huruf'),
+  password: z.string({ message: 'Password wajib diisi' }).min(8, 'Password minimal 8 karakter'),
+  name: z.string({ message: 'Name wajib diisi' }).min(5, 'Name minimal 5 huruf')
 });
 
 export const loginSchema = z.object({
-  username: z.string({ required_error: 'Username wajib diisi.' }),
-  password: z.string({ required_error: 'Password wajib diisi.' })
+  username: z.string({ message: 'Username wajib diisi.' }),
+  password: z.string({ message: 'Password wajib diisi.' })
 });
