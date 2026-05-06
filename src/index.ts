c6de9e9391f6439f52c62d9aed10d64c9d33b5c0
Ibnu Alwan Maulana <onokosy@hotmail.com>
2026-05-06 16:49:50 +0700

    test deploy

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.get('/health', async (req, res) => {
         await prisma.$connect();
         res.json({ status: 'OK', db: 'Connected' });
     } catch(err) {
-        res.status(500).json({ status: 'Error', db: 'disconnect' });
+        res.status(500).json({ status: 'Error', db: 'disconnectt' });
     } finally {
         await prisma.$disconnect();
     }
